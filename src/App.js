5a660a387cf035e19ea4fbc32754e430e4681e71
Thandar-Lin11 <thandarlinttu@gmail.com>
2023-08-15 11:13:43 +0630

    react-movie-app

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+const API_URL = "http://www.omdbapi.com?apikey=8d0cec72";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+    console.log(data);
+  };
+
+  return (
+    <div className="app">
+      <h1>Majestic Movieplex </h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
